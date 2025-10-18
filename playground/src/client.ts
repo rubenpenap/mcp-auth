fe36f862a41cc1abd22a4bc7ec4650806e03822d
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-10-18 15:57:10 -0300

    refactor: update getClient function to accept oauthToken parameter
    - Modified the getClient function to accept an oauthToken string parameter for initializing the DBClient.
    - Passed the oauthToken to the DBClient constructor, enhancing the client’s ability to handle authentication.

--- playground/src/client.ts
+++ playground/src/client.ts
@@ -2,8 +2,6 @@ import { DBClient } from '@epic-web/epicme-db-client'
 
 export const EPIC_ME_AUTH_SERVER_URL = 'http://localhost:7788'
 
-// 🐨 accept an oauthToken string parameter
-export function getClient() {
-	// 🐨 pass the oauthToken to the DBClient constructor as the second argument
-	return new DBClient(EPIC_ME_AUTH_SERVER_URL)
+export function getClient(oauthToken: string) {
+	return new DBClient(EPIC_ME_AUTH_SERVER_URL, oauthToken)
 }
